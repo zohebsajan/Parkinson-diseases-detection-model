77d94bb26c198887533b13a438e3ffa74ab01527
zohebsajan <125746023+zohebsajan@users.noreply.github.com>
2025-08-06 12:41:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,111 @@
+# Importing the Dependencies
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# loading the data from csv file to a Pandas DataFrame
+parkinsons_data = pd.read_csv('archive\parkinsons.csv')
+
+# printing the first 5 rows of the dataframe
+print(parkinsons_data.head())
+
+# number of rows and columns in the dataframe
+parkinsons_data.shape
+
+# getting more information about the dataset
+parkinsons_data.info()
+
+# checking for missing values in each column
+parkinsons_data.isnull().sum()
+
+# getting some statistical measures about the data
+parkinsons_data.describe()
+
+# distribution of target Variable
+parkinsons_data['status'].value_counts()
+
+# 1  --> Parkinson's Positive
+
+# 0 --> Healthy
+
+
+# Grouping the data based on the target variable and computing the mean for numeric columns
+numeric_columns = parkinsons_data.select_dtypes(include=np.number).columns
+print(parkinsons_data.groupby('status')[numeric_columns].mean())
+
+# Data Pre-Processing
+
+# Separating the features & Target
+X = parkinsons_data.drop(columns=['name','status'], axis=1)
+# axis=1 for cloumns and 0 for rows
+Y = parkinsons_data['status']
+print(X)
+
+print(Y)
+
+# Splitting the data to training data & Test data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
+
+# Data standardization
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+#as we don't want our system to know about train data we don't fit test 
+# this is will standardize according to x_train and transform based on the scaler 
+
+X_train = scaler.transform(X_train)
+
+X_test = scaler.transform(X_test)   
+
+print(X_train)
+
+# Support Vector Machine Model
+model = svm.SVC(kernel='linear')
+
+# training the SVM model with training data
+model.fit(X_train, Y_train)
+
+# Accuracy Score
+# accuracy score on training data
+X_train_prediction = model.predict(X_train)
+training_data_accuracy = accuracy_score(Y_train, X_train_prediction)
+print('Accuracy score of training data : ', training_data_accuracy)
+
+# accuracy score on training data
+X_test_prediction = model.predict(X_test)
+test_data_accuracy = accuracy_score(Y_test, X_test_prediction)
+print('Accuracy score of test data : ', test_data_accuracy)
+
+
+input_data = (197.07600,206.89600,192.05500,0.00289,0.00001,0.00166,0.00168,0.00498,0.01098,0.09700,0.00563,0.00680,0.00802,0.01689,0.00339,26.77500,0.422229,0.741367,-7.348300,0.177551,1.743867,0.085569)
+
+# changing input data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the numpy array
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# standardize the data
+std_data = scaler.transform(input_data_reshaped)
+
+prediction = model.predict(std_data)
+print(prediction)
+
+
+if (prediction[0] == 0):
+  print("The Person does not have Parkinsons Disease")
+
+else:
+  print("The Person has Parkinsons")
+
+import warnings
+
+# Suppress specific warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    std_data = scaler.transform(input_data_reshaped)
